08e433044b7e158b5869fee1b9726eed3adbc25f
MauryCB21 <clementebarraganmauricio21@gmail.com>
2024-10-01 05:53:02 +0000

    script

---
+++ alumno.sh
@@ -0,0 +1,11 @@
+read -p "ingrese su nombre:" nombre
+read -p "ingrese su edad:" edad
+read -p "ingrese su matricula:" matricula
+read -p " ingrese su carrera:" carrera
+
+echo "Nombre: Mauricio Clemente Barragan > alumno.txt
+echo "Edad: 21 años " >> alumno.txt
+echo "Matricula: 10056428" >> alumno.txt
+echo "Carrera: Informatica" >> alumno.txt
+
+echo "Datos guardados en alumno.txt"
